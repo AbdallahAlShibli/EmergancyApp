2c96e0942e8078f3b1954df8bd83e06a95e54196
AbdallahAlShibli <48906718+AbdallahAlShibli@users.noreply.github.com>
2022-04-09 12:42:36 +0400

    update 1

--- app/src/main/java/com/tarek/emergancyapp/MainActivity.java
+++ app/src/main/java/com/tarek/emergancyapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.tarek.emergancyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,6 +13,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,14 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         emergency = findViewById(R.id.emergency_call);
         emergency.setOnClickListener(view -> {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            try {
+                try {
+                    mediaPlayer.setDataSource("file:///assets/alarm.wav");
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    mediaPlayer.setLooping(true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
 
             CollectionReference userData = Ff.collection("status");
             Map<String, Object> data1 = new HashMap<>();
             data1.put("emergencyCall", FU.getUid());
 
             try {
-                Thread.sleep(1000);
-
+                Thread.sleep(4000);
+                mediaPlayer.stop();
                 userData.document(FA.getUid()).set(data1);
                 Toast.makeText(getApplicationContext(), "Alert sent to the hospital!", Toast.LENGTH_SHORT).show();
 
