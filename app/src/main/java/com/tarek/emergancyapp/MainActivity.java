bc41220b4d7ff5cbfa8f0d922764eacf4a8ea1ee
AbdallahAlShibli <48906718+AbdallahAlShibli@users.noreply.github.com>
2022-04-09 12:20:52 +0400

    update 1

--- app/src/main/java/com/tarek/emergancyapp/MainActivity.java
+++ app/src/main/java/com/tarek/emergancyapp/MainActivity.java
@@ -2,14 +2,25 @@ package com.tarek.emergancyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private MaterialButton emergency;
+    private FirebaseFirestore Ff = FirebaseFirestore.getInstance();
+    private FirebaseAuth FA = FirebaseAuth.getInstance();
+    private FirebaseUser FU = FA.getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +29,22 @@ public class MainActivity extends AppCompatActivity {
 
         emergency = findViewById(R.id.emergency_call);
         emergency.setOnClickListener(view -> {
-            Toast.makeText(getApplicationContext(), "Alert sent to the hospital!", Toast.LENGTH_SHORT).show();
+
+            CollectionReference userData = Ff.collection("status");
+            Map<String, Object> data1 = new HashMap<>();
+            data1.put("emergencyCall", FU.getUid());
+
+            try {
+                Thread.sleep(1000);
+
+                userData.document(FA.getUid()).set(data1);
+                Toast.makeText(getApplicationContext(), "Alert sent to the hospital!", Toast.LENGTH_SHORT).show();
+
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+
+
         });
     }
 }
